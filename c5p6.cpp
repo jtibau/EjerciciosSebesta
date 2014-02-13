ba108c3ceaaa184b8627c50a3ec2da088c01c636
unknown <gaumala@espol.edu.ec>
2014-02-12 21:31:46 -0500

    Corregidos varios errores Se reescribió la pregunta 4 del capítulo 5 con un formato más agradable. Ahora contiene listings y el código se ajusta al tamaño de la página. También se pusieron más ejemplos. Se corrigió un error en el código C++ en la pregunta 6 del capítulo 5. Las tildes y otros carácteres que tuvieron que ser borrados no se reconocían correctamente. Se corrigió un pequeño error en un listing del capítulo 7 pregunta 5.

--- c5p6.cpp
+++ c5p6.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main(){
-  for (int i = 0 ; i<10; i++){
-    cout << i;
-  }
-  cout << i; //Intentar acceder a variable i
-  return 0;
+	for(int i = 0; i < 10; i++){
+		cout << i;
+	}
+	cout <<i; //Intentar acceder a variable i
+return 0;
 }
 
-//Error
-//c5p6.cpp: En la función int main():
-//c5p6.cpp:8:11: error: la búsqueda de nombre de 'i'
-//cambió por el nuevo alcance ISO de 'for'
-//[-fpermissive]
+//c5p6.cpp: en la funcion int main():
+//c5p6.cpp:8:11 error: la busqueda del nombre de 'i'
+//cambio por el nuevo alcance ISO de 'for'
--- c5p6.cs
+++ c5p6.cs
@@ -1,8 +1,7 @@
-using System;
 
+using System;
 class c5p6
-{
-  static void Main()
+{  static void Main()
   {
     for(int i = 0 ; i < 10 ; i++)
       {
@@ -10,8 +9,6 @@ class c5p6
       }
     Console.WriteLine(i); //Intentando acceder a variable i
   }
-
 }
-
 //c5p6.cs(11,23): error CS0103: The name `i' does 
 //not exist in the current context
